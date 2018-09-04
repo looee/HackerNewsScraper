70a75442e7d9c3e2a5f01a996b7a160fb6ccfffa
Louis Varney <looee@users.noreply.github.com>
2018-09-04 11:10:58 +0100

    Added in the first class

---
+++ ClassLibrary/HackerNewsOutput.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary
+{
+    class HackerNewsOutput
+    {
+            public string Title { get; set; }
+            public string URI { get; set; }
+            public string Author { get; set; }
+            public int Points { get; set; }
+            public int Comments { get; set; }
+            public int Rank { get; set; }    
+    }
+}
